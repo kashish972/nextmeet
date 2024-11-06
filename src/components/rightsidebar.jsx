0464f95c8850d98c42c984b28b65d14d68986784
Kashish Arora <kashisharorasahib@gmail.com>
2024-11-06 20:01:09 +0530

    new

--- src/components/rightsidebar.jsx
+++ src/components/rightsidebar.jsx
@@ -2,6 +2,7 @@ import { Search, Plus, MoreHorizontal } from "lucide-react";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
+
 const friendsData = [
   { name: "John Doe", time: "2min", online: true },
   { name: "Sarah Smith", time: "", online: false },
@@ -31,9 +32,11 @@ const RightSidebar = () => {
       <div className="grid grid-cols-3 gap-2 mb-6">
         {/* First Photo with Plus Icon */}
         <div className="relative">
-          <image
+          <Image
             src="https://imgs.search.brave.com/HHqp0H51nRmTpTmMoyv1cchnINRZf5MTfu8ggzqY_rM/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2IxLzdh/L2U2L2IxN2FlNmFj/M2UyNjhiYWE1OTc4/MmM5MmFiNDhjNDk0/LmpwZw"
             alt="Photo 1"
+            width={100}
+            height={100}
             className="w-full h-24 object-cover rounded-full"
           />
           <Plus className="absolute bottom-2 right-2 h-5 w-5 bg-white rounded-full p-1" />
@@ -42,9 +45,11 @@ const RightSidebar = () => {
         {/* Other Photos with Green Border and Rounded Images */}
         {[2, 3].map((num) => (
           <div key={num} className="border-2 border-green-500 rounded-lg">
-            <image
+            <Image
               src={`https://imgs.search.brave.com/yI_NQsUIfbdG2mn4MZMvdO5omxXAQFxetSqG2TqerZk/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNy8w/Mi8xNi8yMy8xMC9z/bWlsZS0yMDcyOTA3/XzY0MC5qcGc`}
               alt={`Photo ${num}`}
+              width={100}
+              height={100}
               className="w-full h-24 object-cover rounded-full"
             />
           </div>
@@ -64,7 +69,10 @@ const RightSidebar = () => {
             <div key={index} className="flex items-center justify-between">
               <div className="flex items-center gap-3">
                 <Avatar className="h-8 w-8">
-                  <AvatarImage src="https://imgs.search.brave.com/i9RDtXHhCT6JBeH0Wf-D9DMLf3zYKPIUek6zwS2v6Zk/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/NTcxMjgzOTgtZTA3/NmE2YzY0ZDVmP2Zt/PWpwZyZxPTYwJnc9/MzAwMCZpeGxpYj1y/Yi00LjAuMw.jpeg"/>
+                  <AvatarImage
+                    src="https://imgs.search.brave.com/i9RDtXHhCT6JBeH0Wf-D9DMLf3zYKPIUek6zwS2v6Zk/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE1/NTcxMjgzOTgtZTA3/NmE2YzY0ZDVmP2Zt/PWpwZyZxPTYwJnc9/MzAwMCZpeGxpYj1y/Yi00LjAuMw.jpeg"
+                    alt="Friend Avatar"
+                  />
                 </Avatar>
                 <span className="text-sm">{friend.name}</span>
               </div>
